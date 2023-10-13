d521777c8a45c0498ef06addb4969c0c003fa6c2
EfeKardes <134795966+EfeKardes0@users.noreply.github.com>
2023-10-13 20:41:55 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,33 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Uygulama1());
+  runApp(MyColumnApp());
 }
 
-class Uygulama1 extends StatelessWidget {
-  const Uygulama1({super.key});
+class MyColumnApp extends StatelessWidget {
+  const MyColumnApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text("UYGULAMA1")),
-            backgroundColor: Colors.red,
-            body:  Container(
-              alignment: AlignmentDirectional.center,
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.red,
-              child: Text(
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                "Bu ilk uygulamamız ",
-                style: TextStyle(
-                    color: Colors.yellow,
-                    fontSize: 35,
-                    fontWeight: FontWeight.w900),
-              ),
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Top Sellers"),
+        ),
+        body: Container(
+          color: Colors.amber,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              
+              Row
+                (children: [
+                Text("1- "),
+                Expanded(child: Text("Fırat Suyu Kan Akıyor - Yaşar KEMAL    ")),
+                ElevatedButton(onPressed: () {
+
+                }, child: Text("Satın al")),
+                Spacer(),
+                
+              ],), Row
+                (children: [
+                Text("2- "),
+                Expanded(child: Text("Onuncu Köy - Fakir BAYKURT    ")),
+                ElevatedButton(onPressed: () {
+
+                }, child: Text("Satın al")),
+                Spacer(),
+
+              ],), Row
+                (children: [
+                Text("3- "),
+                Expanded(child: Text("Suç Ve Ceza - Dostoyevski     ")),
+                ElevatedButton(onPressed: () {
+
+                }, child: Text("Satın al")),
+                Spacer(),
+
+              ],), Row
+                (children: [
+                Text("4- "),
+                Expanded(child: Text("Beyaz Geceler - Dostoyevski     ")),
+                ElevatedButton(onPressed: () {
+
+                }, child: Text("Satın al")),
+                Spacer(),
+
+              ],), Row
+                (children: [
+                Text("5- "),
+                Expanded(child: Text("Demir Ökçe - Jack LONDON    ")),
+                ElevatedButton(onPressed: () {
+
+                }, child: Text("Satın al")),
+                Spacer(),
+
+              ],)
+
+              /*
+              Text("1- Fırat Suyu Kan Akıyor - Yaşar KEMAL"),
+              Text("2- Onuncu Köy - Fakir BAYKURT"),
+              Text("3- Suç Ve Ceza - Dostoyevski"),
+              Text("4- Beyaz Geceler - Dostoyevski"),
+              Text("5- Demir Ökçe - Jack LONDON"),
+               */
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
